d2ca9e2efe5dcfa9b10a98f89a1803dce6a22714
Nicolasbernard2000 <etu42888@henallux.be>
2021-11-02 17:31:10 +0100

    Add disconnection option

--- app/src/main/java/com/example/smartcity_app/ui/fragment/ProfileFragment.java
+++ app/src/main/java/com/example/smartcity_app/ui/fragment/ProfileFragment.java
@@ -1,10 +1,12 @@
 package com.example.smartcity_app.ui.fragment;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -29,7 +31,9 @@ public class ProfileFragment extends Fragment {
     private EditText city;
     private EditText password;
     private EditText birthDate;
+    private Button button;
     private User user;
+    private ViewGroup container;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -40,42 +44,49 @@ public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.profile_fragment, container, false);
+        this.container = container;
 
         user = MainActivity.getUser();
-        if(user == null) {
-            NavController navController = Navigation.findNavController(container);
-            navController.navigate(R.id.fragment_login);
-        } else {
-            information = (TextView)root.findViewById(R.id.profile_information);
+        information = (TextView)root.findViewById(R.id.profile_information);
 
-            firstName = (EditText) root.findViewById(R.id.profile_edit_first_name);
-            lastName = (EditText) root.findViewById(R.id.profile_edit_last_name);
-            email = (EditText) root.findViewById(R.id.profile_edit_email);
-            street = (EditText) root.findViewById(R.id.profile_edit_street);
-            houseNumber = (EditText) root.findViewById(R.id.profile_edit_house_number);
-            zipCode = (EditText) root.findViewById(R.id.profile_edit_zip_code);
-            city = (EditText) root.findViewById(R.id.profile_edit_city);
-            password = (EditText)root.findViewById(R.id.profile_edit_password);
-            birthDate = (EditText) root.findViewById(R.id.profile_edit_birthdate);
+        firstName = (EditText) root.findViewById(R.id.profile_edit_first_name);
+        lastName = (EditText) root.findViewById(R.id.profile_edit_last_name);
+        email = (EditText) root.findViewById(R.id.profile_edit_email);
+        street = (EditText) root.findViewById(R.id.profile_edit_street);
+        houseNumber = (EditText) root.findViewById(R.id.profile_edit_house_number);
+        zipCode = (EditText) root.findViewById(R.id.profile_edit_zip_code);
+        city = (EditText) root.findViewById(R.id.profile_edit_city);
+        password = (EditText)root.findViewById(R.id.profile_edit_password);
+        birthDate = (EditText) root.findViewById(R.id.profile_edit_birthdate);
+        button = (Button) root.findViewById(R.id.profile_button);
 
-            String info = user.getLastName().toUpperCase() + " " + user.getFirstName();
-            information.setText(info);
-            firstName.setText(user.getFirstName());
-            lastName.setText(user.getLastName());
-            email.setText(user.getEmail());
-            street.setText(user.getStreet());
-            houseNumber.setText(user.getHouseNumber().toString());
-            zipCode.setText(user.getZipCode().toString());
-            city.setText(user.getCity());
-            password.setText(user.getPassword());
-            birthDate.setText(user.getBirthDate().toString());
-        }
+        String info = user.getLastName().toUpperCase() + " " + user.getFirstName();
+        information.setText(info);
+        firstName.setText(user.getFirstName());
+        lastName.setText(user.getLastName());
+        email.setText(user.getEmail());
+        street.setText(user.getStreet());
+        houseNumber.setText(user.getHouseNumber().toString());
+        zipCode.setText(user.getZipCode().toString());
+        city.setText(user.getCity());
+        password.setText(user.getPassword());
+        birthDate.setText(user.getBirthDate().toString());button.setOnClickListener(new DisconnectListener(root.getContext()));
         return root;
     }
 
-    @Override
-    public void onStart() {
-        super.onStart();
+    private class DisconnectListener implements View.OnClickListener {
+        private Context context;
 
+        public DisconnectListener(Context context) {
+            this.context = context;
+        }
+
+        @Override
+        public void onClick(View v) {
+            MainActivity.setUser(null);
+            NavController navController = Navigation.findNavController(container);
+            navController.navigate(R.id.fragment_login);
+        }
     }
+
 }
