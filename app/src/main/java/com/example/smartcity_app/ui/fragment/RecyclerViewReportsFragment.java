bf4e88724c1fdc9bbbc48abe01376d9ba158bde0
ThibBer <47086150+ThibBer@users.noreply.github.com>
2021-11-21 18:29:28 +0100

    Update RecyclerViewReportsFragment.java

--- app/src/main/java/com/example/smartcity_app/ui/fragment/RecyclerViewReportsFragment.java
+++ app/src/main/java/com/example/smartcity_app/ui/fragment/RecyclerViewReportsFragment.java
@@ -105,7 +105,7 @@ public class RecyclerViewReportsFragment extends Fragment {
             String address = report.getStreet() + ", " + report.getHouseNumber() + "\n" + report.getZipCode() + " " + report.getCity();
 
             holder.location.setText(report.getCity());
-            holder.date.setText("Date de merde");
+            holder.date.setText("Date à changer");
             holder.type.setText(report.getReportType().getLabel());
             holder.address.setText(address);
             holder.status.setText(report.getState());
