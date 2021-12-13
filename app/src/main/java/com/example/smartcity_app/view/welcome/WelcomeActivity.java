fc9c74bd3983fc5fd71bd40fdc2840135711746e
Nicolasbernard2000 <nicobern.play@gmail.com>
2021-12-13 18:33:34 +0100

    English Toolbar

--- app/src/main/java/com/example/smartcity_app/view/welcome/WelcomeActivity.java
+++ app/src/main/java/com/example/smartcity_app/view/welcome/WelcomeActivity.java
@@ -1,6 +1,7 @@
 package com.example.smartcity_app.view.welcome;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Context;
 import android.content.Intent;
